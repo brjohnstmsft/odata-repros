9ca287023d0b0308b80770f20f2bab7b98f1ebd0
Bruce Johnston <bruce.johnston@microsoft.com>
2018-09-22 13:47:09 -0700

    Adding workaround to null-in-collection repro

--- NullInComplexCollection/Repro/Startup.cs
+++ NullInComplexCollection/Repro/Startup.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Formatter.Deserialization;
+using Microsoft.AspNet.OData.Routing.Conventions;
+using Microsoft.OData;
 using Microsoft.OData.Edm;
 
 namespace Repro
@@ -12,9 +19,52 @@ namespace Repro
     {
         public static void Configure(HttpConfiguration httpConfig)
         {
+            const string RoutePrefix = "odata";
+            const string RouteName = "odata";
+
             IEdmModel model = Model.Build();
-            httpConfig.MapODataServiceRoute("odata", "odata", model);
+            httpConfig.MapODataServiceRoute(
+                RoutePrefix,
+                RouteName,
+                builder =>
+                {
+                    builder
+                        .AddService(ServiceLifetime.Singleton, sp => model)
+
+                        // Uncomment the line below to test the workaround.
+                        ////.AddService<ODataResourceSetDeserializer, CustomResourceSetDeserializer>(ServiceLifetime.Singleton)
+                        .AddService<IEnumerable<IODataRoutingConvention>>(ServiceLifetime.Singleton, sp => ODataRoutingConventions.CreateDefault());
+                });
+
             httpConfig.Filters.Add(new ActionArgumentValidationAttribute());
+        }
+
+        private sealed class CustomResourceSetDeserializer : ODataResourceSetDeserializer
+        {
+            public CustomResourceSetDeserializer(ODataDeserializerProvider deserializerProvider) : base(deserializerProvider)
+            {
+            }
+
+            public override IEnumerable ReadResourceSet(
+                ODataResourceSetWrapper resourceSet,
+                IEdmStructuredTypeReference elementType,
+                ODataDeserializerContext readContext)
+            {
+                if (elementType.IsComplex() && !elementType.IsNullable &&
+                    resourceSet.Resources.Any(r => r == null))
+                {
+                    string typeRefString =
+                        $"{elementType.FullName()}[Nullable={elementType.IsNullable}]";
+
+                    string message =
+                        $"A null value was found with the expected type '{typeRefString}'. The expected type " +
+                        $"'{typeRefString}' does not allow null values.";
+
+                    throw new ODataException(message);
+                }
+
+                return base.ReadResourceSet(resourceSet, elementType, readContext);
+            }
         }
 
         private sealed class ActionArgumentValidationAttribute : ActionFilterAttribute
